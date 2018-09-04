4f5bddd22871c8e2dc0a328136ee8b67eccb38c1
Nam <nam.ho@tekcent.com>
2018-09-04 12:51:12 +0700

    nam.ho first commit

---
+++ nam.ho.local/nam.ho.local.Tests/Test1.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace nam.ho.local.Tests
+{
+    public class Test1
+    {
+        [Fact]
+        public void FailedTest()
+        {
+            Assert.True(false);
+        }
+    }
+}
---
+++ nam.ho.local/nam.ho.local/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="nam.ho.local.MvcApplication" Language="C#" %>
